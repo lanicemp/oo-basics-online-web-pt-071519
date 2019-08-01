4dffebe50a9fd9027c717dda09f577426937c8ea
LaNice Powell <lanicemp@aol.com>
2019-08-01 16:10:42 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,5 +1,8 @@
 class Book 
   
+  attr_accessor :author, :page_count, :genre
+  #added the attribute accessor and was able to note out the other setter and getter 
+  
   def initialize(title)
     @title = title 
   end 
@@ -8,29 +11,30 @@ class Book
     @title 
   end 
   
-  def author=(author)
-    @author = author
-  end 
   
-  def author
-    @author 
-  end 
+  #def author=(author)
+   # @author = author
+  #end 
   
-  def page_count=(page_count)
-    @page_count = page_count 
-  end 
+  #def author
+   # @author 
+  #end 
   
-  def page_count
-    @page_count
-  end 
+  #def page_count=(page_count)
+   # @page_count = page_count 
+  #end 
+  
+  #def page_count
+   # @page_count
+  #end 
     
-    def genre=(genre)
-      @genre = genre
-    end 
+   # def genre=(genre)
+    #  @genre = genre
+    #end 
     
-    def genre 
-      @genre
-    end 
+    #def genre 
+     # @genre
+    #end 
    
    def turn_page 
      puts "Flipping the page...wow, you read fast!"
